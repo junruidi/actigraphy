cefb34729f618619885462fe30b08326d17afaea
Junrui DI <jdi2@jhu.edu>
2018-06-08 11:14:20 -0400

    a

---
+++ R/IS.R
@@ -0,0 +1,47 @@
+#' @title Interdaily Statbility
+#' @description This function calcualte interdaily stability, a nonparametric metric
+#' of circadian rhtymicity
+#'
+#' @param x  \code{data.frame} of dimension p by 1440, representing the p days of activity data for one subjectect
+#' @param level time resolution to calcualte IS. Can be either "minute", or "hour".
+#' @return IS
+#'
+#'
+#' @export
+#'
+#'
+#' @examples
+#' data(example_activity_data)
+#' count1 = c(t(example_activity_data$count[c(1,2,3),-c(1,2)]))
+#' is = IS(x = count1, level = "hour")
+#'
+#'
+
+IS = function(
+  x,
+  level = c("minute","hour")
+){
+  level = match.arg(level)
+  if(level == "hour"){
+    x = convert_2_hr(x)
+  }
+  p = ncol(x)
+  hr_mean = colMeans(x)
+  v = c(t(x))
+  n = length(v)
+  numerator = sum((hr_mean - mean(v))^2)/p
+  denominator = sum((v - mean(v))^2)/n
+  return(numerator/denominator)
+}
+
+convert_2_hr = function(x){
+  hr = NULL
+  for(i in 1:24){
+    temp = x[,(60*(i-1)+1):(60*i)]
+    temp.i = rowSums(temp)
+    hr = cbind(hr,temp.i)
+  }
+  hr = data.frame(hr)
+  names(hr) = paste0("hr",c(1:24))
+  return(hr)
+}
