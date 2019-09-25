f3403347f03a9f8a1acaca3e5792466be114a580
Junrui Di <jdi2@jhu.edu>
2019-09-24 22:02:01 -0400

    fix addtional reshape problem

--- R/fragmentation_long.R
+++ R/fragmentation_long.R
@@ -145,14 +145,14 @@ fragmentation_long = function(
   if(by == "subject"){
 
     long.count = reshape(count.data, varying = names(count.data)[3:1442],direction = "long",
-                         timevar = "MIN",idvar = c("ID","Day"),v.names = "values")
+                         timevar = "MIN",idvar = c("ID","Day"),v.names = "values",new.row.names = c(1:(1440*nrow(count.data))))
     long.count = long.count[
       with(long.count, order(ID, Day,MIN)),
       ]
 
 
     long.wear = reshape(weartime, varying = names(weartime)[3:1442],direction = "long",
-                         timevar = "MIN",idvar = c("ID","Day"),v.names = "values")
+                         timevar = "MIN",idvar = c("ID","Day"),v.names = "values",new.row.names = c(1:(1440*nrow(count.data))))
     long.wear= long.wear[
       with(long.wear, order(ID, Day,MIN)),
       ]
